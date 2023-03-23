4af1a4116a24048d6ab476f97c848f04dc0e7677
vguy-deroubaix <valentin.gdx@gmail.com>
2023-03-23 09:53:25 +0100

    modif de type

--- machine_a_registre.ml
+++ machine_a_registre.ml
@@ -44,23 +44,34 @@ let clear registers n = (Register.update n (fun e -> match e with
                                                     | Some e -> (Some 0)) registers)                            
 ;;
 
-let jump (Exit n) = ;;
+let jump (Exit n) = (Exit n) ;;
 
-type instruction =Inc of ((int Register.t)->label->(int Register.t)) 
-                  | Dec of ((int Register.t)->label->(int Register.t)) 
-                  | Clear of ((int Register.t)->label->(int Register.t)) 
-                  | Copy of ((int Register.t) ->label->label->(int Register.t))
-                  | Jump of (exit-> instruction)
-                  | JumpM of (label->exit->instruction) ;;
+let jumpM register m (Exit n) = if (Register.find m register) = 0 then n else  ;;
 
-
-type param = LabelParam of label 
-            | LabelCouple of (label*label) 
+type param = LabelParam of ((int Register.t)*label) 
+            | LabelCouple of ((int Register.t)*label*label) 
             | Exitparam of exit
             | LabelExCouple of (label*exit)
 ;;
 
+type instruction = Mono1S of (LabelParam->(int Register.t)) (*un parametre une sortie*)
+                  | Duo1S of (LabelCouple->(int Register.t)) (*deux parametres une sortie*)
+                  | Mono2S of (Exitparam->exit) (*un parametre deux sorties*)
+                  | Duo2S of (LabelExCouple->exit) (*deux parametres deux sorties*)
+;;
+
+
 type programm = Programm of (instruction*param) list;;
 
 
-let execution (Programm programme) (Register registre) = ;;
+let rec execution (Programm programme) registre = 
+  match programme with 
+    | [] -> []
+    | (instruction,param)::t -> begin match instruction with
+                                  | Mono1S(instruction) -> 
+                                  | Duo1S(instruction)
+                                  | Mono2S(instruction)
+                                  | Duo2S(instruction)
+
+                                   
+;;
