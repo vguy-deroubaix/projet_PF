b8a7734ece55cc0b9dd0404f9a8521b36112949e
vguy-deroubaix <valentin.gdx@gmail.com>
2023-03-26 18:57:48 +0200

    finition et exemple

--- machine_a_registre.ml
+++ machine_a_registre.ml
@@ -1,7 +1,7 @@
 (*Magine à registre Axel Verneuil, Valentin Guy-Deroubaix*)
 
 
-type label = Label of int;; (*name of the register*)
+type label = Label of int;; (*name of the nth register*)
 
 (*type register = Register of {label:int; integer : int} ;;*)
 
@@ -17,7 +17,6 @@ else
             -1
 ;;
 
-
 module RegistersOrdonee = struct
   type t = label;;
   let compare = compare_registerLabel;;
@@ -27,10 +26,10 @@ module Register = Map.Make(RegistersOrdonee)
 
 type ligne = Ligne of int ;; (*ligne number*)
 
-type param = LabelParam of {register : int Register.t ;label : label; ligne : ligne} 
-            | LabelCouple of {label1 : label;label2 : label; ligne :ligne}
+type param = LabelParam of {label : label} 
+            | LabelCouple of {label1 : label;label2 : label}
             | Exitparam of {ligne : ligne}
-            | LabelExCouple of {label1 : label; ligneN :ligne; ligne :ligne}
+            | LabelExCouple of {label : label; ligneN :ligne}
 ;;
 
 type instruction = Mono1S of (int Register.t-> label -> ligne->int Register.t*ligne) (*un parametre une sortie*)
@@ -59,7 +58,8 @@ let clear registers n (Ligne ligne) = ((Register.update n (fun e -> match e with
 
 let jump (Ligne n) = (Ligne n) ;;
 
-let jumpM register m (Ligne n) (Ligne ligne)= if (Register.find m register) = 0 then (Ligne n) else Ligne (ligne+1) ;;
+let jumpM register m (Ligne n) (Ligne ligne) = if (Register.find m register) = 0 then (Ligne n) else Ligne (ligne+1) 
+;;
 
 
 let compare_line (n :ligne) (m :ligne) = 
@@ -81,6 +81,10 @@ end
 
 module Programm = Map.Make(InstructionOrdonnee)
 
+let afficher_registre registre =
+  Register.iter (fun key a -> print_int a) registre
+;;
+
 let execution programm register = 
   let arret ligne programm = not (Programm.mem ligne programm)
   in
@@ -90,16 +94,34 @@ let execution programm register =
         registre
       else
         match (Programm.find (Ligne ligne) programm) with 
-            | (Mono1S instruction,LabelParam param) -> let (regis,ligne) = (instruction registre param.label param.ligne) in
-                                                      let fin = arret ligne programme in 
-                                                      execution_aux ligne programm regis fin    
-            | (Duo1S instruction,LabelCouple param) -> let (regis,ligne) = (instruction registre param.label1 param.label2 param.ligne) in
-                                                      let fin = arret ligne programme in 
-                                                      execution_aux ligne programm registre fin
-            | (Mono2S instruction,Exitparam param) -> let fin = arret param.ligne programm in 
+            | (Mono1S instruction,LabelParam param) -> let (regis,nextligne) = (instruction registre param.label (Ligne ligne)) in
+                                                      let fin = arret nextligne programme in 
+                                                      execution_aux nextligne programm regis fin    
+            | (Duo1S instruction,LabelCouple param) -> let (regis,nextligne) = (instruction registre param.label1 param.label2 (Ligne ligne)) in
+                                                      let fin = arret nextligne programme in 
+                                                      execution_aux nextligne programm registre fin
+            | (Mono2S instruction,Exitparam param) -> let fin = arret (Ligne ligne) programm in 
                                                       execution_aux (instruction param.ligne) programm registre fin
-            | (Duo2S instruction,LabelExCouple param) -> let fin = arret param.ligne programm in
-                                                        execution_aux (instruction registre param.label1 param.ligneN param.ligne) programm registre fin
+            | (Duo2S instruction,LabelExCouple param) -> let fin = arret (Ligne ligne) programm in
+                                                        execution_aux (instruction registre param.label param.ligneN (Ligne ligne)) programm registre fin
             | (_) -> failwith("crash")  
   in execution_aux (Ligne 1) programm register false
 ;;
+
+
+let registre = Register.empty;;
+
+let registre = Register.add (Label 1) 3 registre;;
+let registre = Register.add (Label 2) 3 registre;;
+let registre = Register.add (Label 3) 0 registre;;
+
+let add = Programm.add (Ligne 1) (Duo2S jumpM,LabelExCouple {label = (Label 1);ligneN = (Ligne 5)}) Programm.empty;;
+
+let add = Programm.add (Ligne 2) (Mono1S inc,LabelParam {label = (Label 2)}) add;;
+
+let add = Programm.add (Ligne 3) (Mono1S dec,LabelParam {label = (Label 1)}) add;;
+
+let add = Programm.add (Ligne 4) (Mono2S jump,Exitparam {ligne = (Ligne 1)}) add;; (*addition*)
+
+
+let regsitre = execution add registre;;
